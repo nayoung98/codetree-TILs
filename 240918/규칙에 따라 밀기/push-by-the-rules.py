b3997f6bf2de6335704e0ef22ce75a7568339281
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-09-18 01:35:00 +0900

    Title: 규칙에 따라 밀기, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240918/규칙에 따라 밀기/push-by-the-rules.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+A = input().rstrip()
+rules = input().rstrip()
+rule_cnt = 0
+
+for rule in rules:
+    if rule == 'L':
+        rule_cnt -= 1
+    else:
+        rule_cnt += 1
+
+# print(A[rule_cnt:])
+# print(A[:rule_cnt])
+
+if rule_cnt < 0:
+    rule_cnt = abs(rule_cnt)
+    print(A[rule_cnt:] + A[:rule_cnt])
+elif rule_cnt > 0:
+    print(A[:rule_cnt] + A[rule_cnt:])
+else:
+    print(A)
