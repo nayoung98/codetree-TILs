128c92993f14f203cc61c3881404c7c1fcfa1ddd
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-09-18 02:31:41 +0900

    Title: 격자 모양 출력하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240918/격자 모양 출력하기/print-grid-shape.py
+++ 240918/격자 모양 출력하기/print-grid-shape.py
@@ -5,11 +5,10 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(m)]
 chk = [[0] * n for _ in range(n)]
 
-for row in range(n):
-    for col in range(1):
-        row_idx = grid[row][col] - 1
-        col_idx = grid[row][col+1] - 1
-        chk[row_idx][col_idx] = (row_idx + 1) * (col_idx + 1)
+for row in range(m):
+    row_idx = grid[row][0] - 1
+    col_idx = grid[row][1] - 1
+    chk[row_idx][col_idx] = (row_idx + 1) * (col_idx + 1)
 
 for row in range(n):
     for col in range(n):
