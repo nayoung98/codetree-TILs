e5e98d506a5dca083b596a210da61d3af4a1a169
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-09-25 00:11:21 +0900

    Title: 그 요일은, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240925/그 요일은/the-day-of-the-day.py
+++ 240925/그 요일은/the-day-of-the-day.py
@@ -8,6 +8,8 @@ days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 target_day_idx = days.index(A)
 
 current_day_idx = 0 
+current_month = m1
+current_day = d1
 cnt = 0
 
 def chk_month(m1):
@@ -18,16 +20,16 @@ def chk_month(m1):
     else:
         return 30
 
-while not (m1 == m2 and d1 == d2+1):
+while not (current_month == m2 and current_day == d2 + 1):
 
-    d1 += 1
-    current_day_idx = (current_day_idx + 1) % 7 
-
-    if target_day_idx == current_day_idx:
+    if current_day_idx == target_day_idx:
         cnt += 1
-        
-    if d1 > chk_month(m1):
-        m1 += 1
-        d1 = 1
-        
+
+    current_day += 1
+    current_day_idx = (current_day_idx + 1) % 7  
+
+    if current_day > chk_month(m1):
+        current_day = 1
+        current_month += 1
+
 print(cnt)
