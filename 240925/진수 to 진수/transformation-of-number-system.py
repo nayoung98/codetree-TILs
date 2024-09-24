6f9cd4be8ca273a90d55e249a5e73426cc09f5a6
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-09-25 00:30:03 +0900

    Title: 진수 to 진수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240925/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+a, b = map(int, input().split())
+origin_n = input().rstrip()
+n = int(origin_n)
+
+# a진수 -> 10진수
+num = 0
+for i in range(len(origin_n)-1, -1, -1):
+   num += a ** i
+print(num)
+
+# 10진수 -> b진수
+ans_list = []
+while True:
+    ans_list.append(num % b)
+    num //= b
+
+    if num == 1:
+        ans_list.append(num)
+        break
+
+for num in ans_list:
+    print(num, end='')
