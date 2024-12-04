e2c33a227823f2aa123abca4289220ba5331b9c0
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-12-04 20:06:39 +0900

    Title: 특정 구간의 원소 평균값, Time: 62ms, Memory: 17MB, Status: Passed - CodeTree

--- 241204/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 241204/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -1,32 +1,23 @@
-# 구간을 잘 골라 -> 모든 구간 정해야됨
-# 해당 구간 내 숫자 합이 최대가 되도록
-
-arr = [4, -5, 8, -1, -6, 9]
-max_sum = 0
-for i in range(n):
-    for j in range(i, n): # 구간 잡기
-        sum_val = 0
-
-        for k in range(i, j + 1):
-            sum_val += arr[k]
-
-        max_sum = max(max_sum, sum_val)
-
 # n개의 숫자
 # 특정 구간 내 원소들의 평균값이 그 구간의 원소 중 하나가 되는 가짓수 구하기
 n = int(input())
 arr = list(map(int, input().split()))
 
 cnt = 0
-for i in range(n):
-    for j in range(i, n):
+for i in range(n): # 구간의 시작점
+    for j in range(i, n): # 구간의 끝점
+        # 구간 [i, j] 내 원소의 합
         tmp_sum = 0
         for k in range(i, j + 1):
             tmp_sum += arr[k]
-        tmp_sum //= abs(i - j + 1)
+        tmp_sum /= (j - i + 1)
         
+        chk = False
         for k in range(i, j + 1):
             if tmp_sum == arr[k]:
-                cnt += 1
+                chk = True
+            
+        if chk:
+            cnt += 1
 
-print(cnt)
+print(cnt)  
