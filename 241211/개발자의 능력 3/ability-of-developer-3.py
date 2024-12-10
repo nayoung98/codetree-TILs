ec1422c99bdbff5dbcf4d864f4f65efb79044c5f
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-12-11 00:53:43 +0900

    Title: 개발자의 능력 3, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241211/개발자의 능력 3/ability-of-developer-3.py
+++ 241211/개발자의 능력 3/ability-of-developer-3.py
@@ -4,19 +4,19 @@
 
 power_list = list(map(int, input().split()))
 
-def get_diff(i, j):
-    sum1 = power_list[i] + power_list[j]
+def get_diff(i, j, k):
+    sum1 = power_list[i] + power_list[j] + power_list[k]
     sum2 = sum(power_list) - sum1
 
     diff = abs(sum1 - sum2)
 
     return diff
 
-
 min_power = 1000000 * 6
 for i in range(len(power_list)):
-    for j in range(i, len(power_list)):
+    for j in range(i + 1, len(power_list)):
+        for k in range(j + 1, len(power_list)):
 
-        min_power = min(min_power, get_diff(i, j))
+            min_power = min(min_power, get_diff(i, j, k))
 
 print(min_power)
