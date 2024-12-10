7b2865f03b5e81c4fade8d455c9d0aa7c5e5cba9
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2024-12-11 00:50:26 +0900

    Title: 개발자의 능력 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241211/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,22 @@
+# 개발자 6명의 알고리즘 능력을 수치화 
+# 6명 -> 3명/ 3명
+# 팀원들의 능력 총합간의 차이를 최소화
+
+power_list = list(map(int, input().split()))
+
+def get_diff(i, j):
+    sum1 = power_list[i] + power_list[j]
+    sum2 = sum(power_list) - sum1
+
+    diff = abs(sum1 - sum2)
+
+    return diff
+
+
+min_power = 1000000 * 6
+for i in range(len(power_list)):
+    for j in range(i, len(power_list)):
+
+        min_power = min(min_power, get_diff(i, j))
+
+print(min_power)
