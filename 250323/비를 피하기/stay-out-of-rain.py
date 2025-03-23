843be17fab8c2220d91eb6e71ee1e8176b4f237c
nayoung98 <92157618+nayoung98@users.noreply.github.com>
2025-03-23 21:08:21 +0900

    Title: 비를 피하기, Time: 110ms, Memory: 21MB, Status: Passed - CodeTree

--- 250323/비를 피하기/stay-out-of-rain.py
+++ 250323/비를 피하기/stay-out-of-rain.py
@@ -8,15 +8,16 @@ from collections import deque
 n, h, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
-time = [[0] * n for _ in range(n)]
+time = [[-1] * n for _ in range(n)]
 q = deque()
 
-# 비를 피할 수 있는 곳 저장
-dest = []
+# 비를 피할 수 있는 곳에서 bfs
 for i in range(n):
     for j in range(n):
         if grid[i][j] == 3:
-            dest.append((i, j))
+            q.append((i, j))
+            visited[i][j] = True
+            time[i][j] = 0
 
 # 격자 내 확인
 def in_range(x, y):
@@ -26,53 +27,28 @@ def in_range(x, y):
 def can_go(x, y):
     return in_range(x, y) and \
             not visited[x][y] and \
-            grid[x][y] != 1 # 0, 3
+            grid[x][y] != 1 and \
+            time[x][y] == -1 # 최단 시간만 기록
 
-# bfs 돌면서 최단 시간(거리) 기록
+# bfs 돌면서 최단 시간 기록
 def bfs():
-    global time, visited
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-    min_result = []
 
     while q:
         x, y = q.popleft()
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
             if can_go(nx, ny):
-
-                time[nx][ny] = time[x][y] + 1
+                
                 visited[nx][ny] = True
                 q.append((nx, ny))
-
-                if grid[nx][ny] == 3:
-                    min_result.append((time[nx][ny]))
-
-    if len(min_result) == 0:
-        return -1
-    
-    return min(min_result)
-                
-# 실행
-result = [[0] * n for _ in range(n)]
-
+                time[nx][ny] = time[x][y] + 1
+bfs()
 for i in range(n):
     for j in range(n):
-
-        # 사람 있는 곳 추가
+        # 사람 있는 곳
         if grid[i][j] == 2:
-            
-            visited[i][j] = True
-            q.append((i, j))
-            
-            # bfs 돌면서 거리 체크
-            result[i][j] = bfs()
-        
-            # 초기화
-            visited = [[False] * n for _ in range(n)]
-            time = [[0] * n for _ in range(n)]
-
-# 출력
-for i in range(n):
-    for j in range(n):
-        print(result[i][j], end = ' ')
+            print(time[i][j], end=' ')
+        else:
+            print(0, end=' ')
     print()
